13aae60482be97cac6478bf57d39b1cd77a26b74
kdavidmitchell <kdavidmitchell@gmail.com>
2018-02-06 10:04:45 -0500

    Character customization code

---
+++ Assets/Alpha/Character Creation/ChangingAppearance.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangingAppearance : MonoBehaviour 
+{
+
+	public SpriteRenderer part;
+	public Sprite[] options;
+	public int index;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		for (int i = 0; i < options.Length; i++)
+		{
+			if (i == index)
+			{
+				part.sprite = options[i];
+			}
+		}	
+	}
+
+	public void Swap()
+	{
+		if (index < options.Length - 1)
+		{
+			index++;
+		} else 
+		{
+			index = 0;
+		}
+	}
+}
---
+++ Assets/Alpha/Character Creation/ChangingColor.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ChangingColor : MonoBehaviour 
+{
+
+	public GameObject panel;
+	public SpriteRenderer head;
+	public Image squareHeadDisplay;
+
+	public Color[] colors;
+
+	public int colorIndex = 1;
+
+	void Update()
+	{
+		squareHeadDisplay.color = head.color;
+
+		for (int i = 0; i < colors.Length; i++)
+		{
+			if (i == colorIndex)
+			{
+				head.color = colors[i];
+			}
+		}
+	}
+
+	public void ChangePanelState(bool state)
+	{
+		panel.SetActive(state);
+	}
+
+	public void ChangeHeadColor(int index)
+	{
+		colorIndex = index;
+	}
+}
