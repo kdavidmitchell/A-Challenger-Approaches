96fa35979b061e5d90bed2ef254c69b0775dff4f
kdavidmitchell <kdavidmitchell@gmail.com>
2018-02-06 15:23:01 -0500

    More stat changes

--- Assets/Alpha/Character Enhancement/StatCollection.cs
+++ Assets/Alpha/Character Enhancement/StatCollection.cs
@@ -17,4 +17,37 @@ public class StatCollection
 	{
 		
 	}
+
+	public bool Contains(StatType statType)
+	{
+		return _statDict.ContainsKey(statType);
+	}
+
+	public Stat GetStat(StatType statType)
+	{
+		if (Contains(statType))
+		{
+			return _statDict[statType];
+		}
+
+		return null;
+	}
+
+	protected Stat CreateStat(StatType statType)
+	{
+		Stat stat = new Stat();
+		_statDict.Add(statType, stat);
+		return stat;
+	}
+
+	protected Stat CreateOrGetStat(StatType statType)
+	{
+		Stat stat = GetStat(statType);
+		if (stat == null)
+		{
+			stat = CreateStat(statType);
+		}
+
+		return stat;
+	}
 }
